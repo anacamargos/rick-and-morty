aba7555d34ed4c03da44df97a08eae66d2e0a56b
anacamargos <alcvlara@sga.pucminas.br>
2020-08-05 08:47:18 -0300

    Creating URLSessionHTTPClient

---
+++ RickAndMorty/RickAndMorty/Sources/Network/URLSessionHTTPClient.swift
@@ -0,0 +1,31 @@
+//
+//  URLSessionHTTPClient.swift
+//  RickAndMorty
+//
+//  Created by Ana Leticia Camargos on 05/08/20.
+//  Copyright © 2020 Ana Leticia Camargos. All rights reserved.
+//
+
+import Foundation
+
+final class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    private struct UnexpectedValuesRepresentation: Error {}
+    
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success((data, response)))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
+            }
+        }.resume()
+    }
+}
